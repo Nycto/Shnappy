bfb77966c13112216139339dd2f6bdc7bd870a6f
Nycto <James@RoundEights.com>
2013-07-28 11:35:09 -0700

    Switch Data over to being an interface

--- src/main/scala/Data.scala
+++ src/main/scala/Data.scala
@@ -13,7 +13,7 @@ import scala.concurrent._
 object Data {
 
     /** Returns a shared data instance */
-    def apply( env: Env, parser: Parser ): Data = new Data(
+    def apply( env: Env, parser: Parser ): Data = new LiveData(
         env.database,
         parser,
         env.couchDB match {
@@ -29,36 +29,55 @@ object Data {
 /**
  * Data access interface
  */
-class Data ( database: String, private val parser: Parser, couch: CouchDB ) {
+trait Data {
+
+    /** Returns the interface for requesting admin data */
+    def admin: AdminData
+
+    /** Shutsdown this connection */
+    def close: Unit
+
+    /** Returns when the SiteData instance given a host name */
+    def forSite( host: String ): Future[Option[SiteData]]
+}
+
+/**
+ * Data access interface
+ */
+class LiveData (
+    database: String, private val parser: Parser, couch: CouchDB
+) extends Data {
 
     // Make sure the database exists
     private val db = couch.db( database )
     db.createNow
 
-    /** Admin data access */
-    val admin = new AdminData( db )
-
     // Design interface
     private val design = Await.result( db.designDir( classOf[Data],
         "siteInfoByHost" -> "/couchdb/siteInfoByHost",
-        "pagesBySlug" -> "/couchdb/pagesBySlug",
-        "pagesByIndex" -> "/couchdb/pagesByIndex",
-        "nav" -> "/couchdb/nav"
+        "pagesBySlug"    -> "/couchdb/pagesBySlug",
+        "pagesByIndex"   -> "/couchdb/pagesByIndex",
+        "nav"            -> "/couchdb/nav"
     ), Duration(3, "second") )
 
-    /** Shutsdown this connection */
+    /** {@inheritDoc} */
+    override val admin = new AdminData( db )
+
+    /** {@inheritDoc} */
     def close: Unit = couch.close
 
-    /** Returns a new Data instance customized for the given host */
-    def forSite( host: String ): Future[Option[SiteData]] = {
-        val cleanHost = SiteInfo.host.process( host ).require.value
-
-        design.view("siteInfoByHost").limit(1).key( cleanHost ).exec.map(
-            _.headOption.map( doc => SiteInfo(doc) ).map( info => {
-                new SiteDataCache( new LiveSiteData(info) )
-            } )
+    /** Fetches down the SiteInfo object for a host name */
+    private def getSiteInfo ( host: String ): Future[Option[SiteInfo]] = {
+        design.view("siteInfoByHost").limit(1).key(
+            SiteInfo.host.process(host).require.value
+        ).exec.map(
+            _.headOption.map( doc =>SiteInfo(doc) )
         )
     }
+
+    /** {@inheritDoc} */
+    override def forSite( host: String ): Future[Option[SiteData]]
+        = getSiteInfo( host ).map( _.map( info => new LiveSiteData(info) ) )
 
     /** Provides request specific data access */
     class LiveSiteData ( override val siteInfo: SiteInfo ) extends SiteData {
@@ -97,8 +116,8 @@ class Data ( database: String, private val parser: Parser, couch: CouchDB ) {
                     )
                 }))
         }
-
     }
 
 }
+
 
