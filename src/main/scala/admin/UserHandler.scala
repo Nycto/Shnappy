78781d259d27d2f6becadd344f88339ef0afb42d
Nycto <James@RoundEights.com>
2013-07-31 18:52:12 -0700

    Move UserEdit to a new file

--- src/main/scala/admin/UserHandler.scala
+++ src/main/scala/admin/UserHandler.scala
@@ -5,50 +5,8 @@ import com.roundeights.skene._
 import com.roundeights.shnappy.Templater
 import com.roundeights.scalon._
 import com.roundeights.attempt._
-import scala.concurrent.Promise
 import java.util.UUID
 
-
-/** Extracts a user being interacted with in the API */
-trait UserEdit {
-
-    /** The user being edited */
-    def userEdit: User
-
-    /** {@inheritDoc} */
-    override def toString = "UserEdit(%s)".format( userEdit )
-}
-
-/**
- * Builds an UserEdit prereq
- */
-class UserEditProvider( val data: AdminData ) extends Provider[UserEdit] {
-
-    /** {@inheritDoc} */
-    override def dependencies: Set[Class[_]] = Set( classOf[Auth] )
-
-    /** {@inheritDoc} */
-    override def build( bundle: Bundle, next: Promise[UserEdit] ): Unit = {
-        for {
-
-            id <- TryTo.except {
-                UUID.fromString( bundle.request.params("userID") )
-            } onFailMatch {
-                case _: Throwable =>
-                    next.failure( new InvalidData("Invalid user ID") )
-            }
-
-            userOpt <- data.getUser(id) :: OnFail.alsoFail(next)
-
-            user <- userOpt :: OnFail {
-                next.failure( new NotFound("User does not exist") )
-            }
-
-        } next.success(new UserEdit {
-            override val userEdit = user
-        })
-    }
-}
 
 
 /**
